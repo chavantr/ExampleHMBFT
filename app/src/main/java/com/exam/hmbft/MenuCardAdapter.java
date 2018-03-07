7709cdde502fb9bcde48e2695e167e74e1b26fdd
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2018-03-07 14:05:19 +0530

    support to menu screen

--- app/src/main/java/com/exam/hmbft/MenuCardAdapter.java
+++ app/src/main/java/com/exam/hmbft/MenuCardAdapter.java
@@ -3,32 +3,67 @@ package com.exam.hmbft;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MenuCardAdapter extends RecyclerView.Adapter<MenuCardAdapter.ViewHolder> {
 
 
+    private LayoutInflater inflater;
+    private ViewHolder viewHolder;
+    private List<Menu> lstMenu;
 
+    public MenuCardAdapter(List<Menu> lstMenu) {
+        this.lstMenu = lstMenu;
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        if (null == inflater) {
+            inflater = LayoutInflater.from(parent.getContext());
+        }
+
+        View view = inflater.inflate(R.layout.layout_cart_row, parent, false);
+
+        viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        viewHolder.lblItemName.setText(lstMenu.get(position).getName());
+        viewHolder.npCount.setDisplayedValues(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"});
+        viewHolder.npCount.setMinValue(1);
+        viewHolder.npCount.setMaxValue(10);
+
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstMenu.size();
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView lblItemName;
+        NumberPicker npCount;
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            lblItemName = itemView.findViewById(R.id.lblItemName);
+            npCount = itemView.findViewById(R.id.npCount);
+
         }
     }
 
--- app/src/main/java/com/exam/hmbft/MenuCartActivty.java
+++ app/src/main/java/com/exam/hmbft/MenuCartActivty.java
@@ -1,18 +1,43 @@
 package com.exam.hmbft;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuCartActivty extends AppCompatActivity {
 
     private RecyclerView lstSelectedItems;
+    private List<Menu> lstMenu = new ArrayList<>();
+    private Button btnOrderBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_cart_activty);
         lstSelectedItems = findViewById(R.id.lstSelectedItems);
+        btnOrderBook = findViewById(R.id.btnBookOrder);
+        btnOrderBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuCartActivty.this, ConfirmationActivity.class);
+                startActivity(intent);
+            }
+        });
+        for (int i = 0; i < SelectMenu.checked.size(); i++) {
+            if (SelectMenu.checked.valueAt(i)) {
+                lstMenu.add(MenuHelper.getMenus().get(MenuHelper.getName()[i]));
+            }
+        }
+
+        lstSelectedItems.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        lstSelectedItems.setAdapter(new MenuCardAdapter(lstMenu));
 
     }
 }
